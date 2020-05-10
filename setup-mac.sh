5fb68043d38e02b18463092d5337d956aa74c29f
Yves Canty <46539628+ycanty@users.noreply.github.com>
2020-05-10 09:24:19 -0400

    Removed setup.yaml

--- setup-mac.sh
+++ setup-mac.sh
@@ -27,31 +27,9 @@ install_ansible() {
     brew install ansible
 }
 
-is_encrypted() {
-    local file="$1"
-
-    cat "${file}" | head -1 | grep "ANSIBLE_VAULT;" > /dev/null 2>&1
-}
-
-
 ANSIBLE_VERSION=${1:-*}
-
-if [[ ! -f setup.yaml ]]
-then
-    echo
-    echo "Please copy setup.yaml.template to setup.yaml,"
-    echo "fill in the user-specific configuration values"
-    echo "and run this script again."
-    echo
-    exit 1
-fi
-
-SETUP_FILE="${SCRIPTDIR}/setup.yaml"
-VAULT_OPTS=""
 
 check_homebrew || install_homebrew
 check_ansible_version "${ANSIBLE_VERSION}" || install_ansible "${ANSIBLE_VERSION}"
 
-is_encrypted "${SETUP_FILE}" && VAULT_OPTS="--vault-id @prompt"
-
-ANSIBLE_CONFIG=${SCRIPTDIR}/ansible.cfg ansible-playbook --extra-vars "@${SETUP_FILE}" ${VAULT_OPTS} -K main.yml
+ANSIBLE_CONFIG=${SCRIPTDIR}/ansible.cfg ansible-playbook -K main.yml
